a809dcb8607b634b41842621abdfc466f60ae7b1
Israel Bazan <ibazan@cimeco.local>
2016-12-01 14:32:43 -0300

    meses en español

--- jtest-001.js
+++ jtest-001.js
@@ -3,7 +3,7 @@ var now = new Date();
 
 var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
 
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+var months = new Array('enero','febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
 
 var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
 
